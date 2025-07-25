203fb5e42e370fb7e491cb7eadd001541c4a8283
Jabanin <45544763+Jabanin@users.noreply.github.com>
2025-07-26 00:07:34 +0200

    Delete gameshelf directory

--- gameshelf/js/login.js
+++
@@ -1,2 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
